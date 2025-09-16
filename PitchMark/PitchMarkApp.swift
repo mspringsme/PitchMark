dcb7d13d73b5337f66ac11150bb55221f7dafd37
Mark Springer <msprings@me.com>
2025-09-16 16:09:32 -0500

    Initial Commit

---
+++ PitchMark/PitchMarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  PitchMarkApp.swift
+//  PitchMark
+//
+//  Created by Mark Springer on 9/16/25.
+//
+
+import SwiftUI
+
+@main
+struct PitchMarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
